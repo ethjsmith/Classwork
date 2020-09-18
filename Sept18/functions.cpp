04888368f8b8ed93a68e1a91eaf302c9b3a387fb
Ethan Smith <ethansmith1@SUU.EDU>
2020-09-18 09:49:05 -0600

    sept 18 work

---
+++ Sept18/functions.cpp
@@ -0,0 +1,13 @@
+/*
+function support file
+*/
+
+int add(int a, int b) {
+	return a+b;
+}
+double add(double x, double y) {
+	return x+y;
+}
+double add(int x, double y) {
+	return x+ y;
+}
---
+++ Sept18/functions.h
@@ -0,0 +1,14 @@
+
+//12. Include a preprocessor directive check
+//13. Compile and test
+
+
+/*
+What should and shouldn't this file contain?
+Why use preprocessor directives instead of boolean variables?
+Why do this at all?
+*/
+
+int add(int,int);
+double add (double,double);
+double add (double, int);
