7667b64707562a536d71c2b48d6a2c478309fec7
Ethan Smith <ejsmith@suu.edu>
2020-10-28 11:50:43 -0600

    "started" on the models, and a navbar

---
+++ gameRec/gameRec/models.py
@@ -0,0 +1,9 @@
+# need to import the DB so that db.model doesn't error :P
+
+class Game(db.Model):
+
+
+class Tag(db.Model):
+
+
+# I think there's some builtin user models... rather not rebuild those because they're a pain
