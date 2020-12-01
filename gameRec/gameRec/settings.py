5d0c216bf3f7e7e629f3024938420e419c541ada
Ethan Smith <ethan@esmithy.net>
2020-11-30 17:52:01 -0700

    added a bunch of GARBAGE

--- gameRec/gameRec/settings.py
+++ gameRec/gameRec/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'o0xxnqj!pl*hw8$v+wp_re)+j)u!itxmh2rgh^kie8u^^wdk21'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1","192.168.0.191","localhost"]
 
 
 # Application definition
@@ -120,3 +121,4 @@ LOGOUT_REDIRECT_URL = "/"
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
